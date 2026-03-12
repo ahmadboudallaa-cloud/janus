5932f52af0abe175449a99d7fb7e5aa10401b67a
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-12 11:25:50 +0000

    chore: remove comments batch 10

--- database/migrations/2026_03_11_100002_create_personal_access_tokens_table.php
+++ database/migrations/2026_03_11_100002_create_personal_access_tokens_table.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+       
+                          
+       
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
@@ -25,9 +25,9 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+       
+                              
+       
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
