8e561c3cb16ff5cdafea4d7fdcc3ae677d8c0673
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2026-03-12 11:25:49 +0000

    chore: remove comments batch 2

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -13,9 +13,9 @@ class LoginRequest extends ApiRequest
         return true;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
+       
+                                   
+       
     public function rules(): array
     {
         return [
--- app/Http/Requests/Auth/LogoutRequest.php
+++ app/Http/Requests/Auth/LogoutRequest.php
@@ -13,9 +13,9 @@ class LogoutRequest extends ApiRequest
         return true;
     }
 
-    /**
-     * @return array<string, mixed>
-     */
+       
+                                   
+       
     public function rules(): array
     {
         return [];
